e22b67d22ffa47b2e05607a855ed2326bc22ecf3
Fyodor Shchukin <f.shchukin@hello.io>
2019-07-04 19:34:14 +0300

    Stupid basic perspective

--- main.py
+++ main.py
@@ -18,22 +18,29 @@ class P:
 
 w, h = 512, 512
 
+# TODO: move buffer ops to file. drawing ops is buffer ops?
 color_buffer = np.zeros((h, w, 3), dtype=np.uint8)
+
+def show_buffer(buffer):
+    img = Image.fromarray(color_buffer, 'RGB')
+    img.show()
 
 def draw_pixel(x, y, color):
     # X and Y are flipped in buffer, so..
-    try:
-        color_buffer[y, x] = color
-    except Exception as ex:
-        print(ex)
+    #try:
+    color_buffer[int(y), int(x)] = color
+    #except Exception as ex:
+    #    print(ex)
 
 def shader(p):
     return (p.x * 255, p.y * 255, 128)
 
-for x in range(w):
-    for y in range(h):
-        draw_pixel(x, y, shader(P(x/w, y/h)))
+def draw_shader(shader):
+    for x in range(w):
+        for y in range(h):
+            draw_pixel(x, y, shader(P(x/w, y/h)))
 
+# draw_shader(shader)
 
 def draw_rect(p1, p2, color):
     maxx = max(p1.x, p2.x)
@@ -55,7 +62,7 @@ def draw_line(p1, p2, color):
     err = dx + dy
     movex, movey = p1.x, p1.y
     while True:
-        draw_pixel(int(movex), int(movey), color)
+        draw_pixel(movex, movey, color)
         erri = err + err
         if erri >= dy:
             err += dy
@@ -92,6 +99,7 @@ def draw_fill_triangle(p1, p2, p3, color):
             if inside:
                 draw_pixel(x, y, color)
     
+# TODO: move test to sep file
 def test_2d_render():
     for i in range(0, 255, 15):
         draw_line(P(10, 10), P(w-10, h-10-i), (0, 0, 128))
@@ -104,24 +112,57 @@ def test_2d_render():
     draw_rect(P(50, 50), P(100, 100), (128, 0, 0))
     draw_pixel(255, 255, (255, 255, 255))
 
-    img = Image.fromarray(color_buffer, 'RGB')
-    img.show()
+    show_buffer(color_buffer)
 
-test_2d_render()
+# test_2d_render()
 
 # Orthographic projection
 # https://en.wikipedia.org/wiki/3D_projection#Orthographic_projection
 cube_points = [
-    P(-1, -1,  1),
-    P( 1, -1,  1),
-    P( 1,  1,  1),
-    P(-1,  1,  1),
+    P(-10, -10,  10),
+    P( 10, -10,  10),
+    P( 10,  10,  10),
+    P(-10,  10,  10),
 
-    P(-1, -1, -1),
-    P( 1, -1, -1),
-    P( 1,  1, -1),
-    P(-1,  1, -1),
+    P(-10, -10,  1),
+    P( 10, -10,  1),
+    P( 10,  10,  1),
+    P(-10,  10,  1),
 ]
+
+def test_ortho_render():
+    def orto(p):
+        scale = [[10, 0, 0], [0, 10, 0]]
+        input = [[p.x], [p.y], [p.z]]
+        offset = [[255], [255]]
+        o = np.matrix(scale) * np.matrix(input) + np.matrix(offset)
+        return P(o[0][0], o[1][0])
+
+    for p in cube_points:
+        op = orto(p)
+        draw_pixel(op.x, op.y, (255, 255, 255))
+
+    show_buffer(color_buffer)
+
+# test_ortho_render()
+
+
+def test_persp_render():
+    # https://en.wikipedia.org/wiki/Camera_matrix
+    def persp(p):
+        fovy = 2
+        xyinput = [[p.x], [p.y]]
+        offset = [[255], [255]]
+        o = fovy/p.z * np.matrix(xyinput) + np.matrix(offset)
+        return P(o[0][0], o[1][0])
+
+    for p in cube_points:
+        op = persp(p)
+        draw_pixel(op.x, op.y, (255, 255, 255))
+
+    show_buffer(color_buffer)
+
+test_persp_render()
 
 
 # Perspective projection
